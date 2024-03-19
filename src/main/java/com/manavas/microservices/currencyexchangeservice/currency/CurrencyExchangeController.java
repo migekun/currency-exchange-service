38c9c769e69aa8cccf30521e78e86a40a2194a2a
Miguel Ángel Navas García <manavas@gmail.com>
2024-03-19 18:55:45 +0100

    micrometer to trace the api

--- src/main/java/com/manavas/microservices/currencyexchangeservice/currency/CurrencyExchangeController.java
+++ src/main/java/com/manavas/microservices/currencyexchangeservice/currency/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.manavas.microservices.currencyexchangeservice.currency;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 public class CurrencyExchangeController {
-
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     private final Environment environment;
 
     private final CurrencyExchangeRepository currencyExchangeRepository;
@@ -21,6 +23,8 @@ public class CurrencyExchangeController {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
+        //2024-03-14T19:01:47.462+01:00  INFO [currency-exchange,80d47dd66fd41d853cb28a5897723d7e,fbdc171a185a9e84] #SB3 2037 --- [currency-exchange] [nio-8000-exec-2] [80d47dd66fd41d853cb28a5897723d7e-fbdc171a185a9e84] c.m.m.c.c.CurrencyExchangeController     : retrieveExchangeValue called with USD to INR
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
         //CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
         Optional<CurrencyExchange> currency = currencyExchangeRepository.findByFromAndTo(from, to);
         CurrencyExchange currencyExchange =
